0f366d61ba1bc759d6837ed17272bb1fcdd34017
comrade-kh <coffeecomrade1@gmail.com>
2020-07-06 23:34:24 +0600

    fix null poster path or backdrop path. set a default image.

--- easyTMDB/src/main/java/com/mythiccosmos/easytmdb/ImageUrl.java
+++ easyTMDB/src/main/java/com/mythiccosmos/easytmdb/ImageUrl.java
@@ -4,6 +4,7 @@ import static com.mythiccosmos.easytmdb.Enum.ImageSize.original;
 
 public class ImageUrl {
     private final String BASE_IMAGE_URL = "http://image.tmdb.org/t/p/";
+    private final String DEFAULT_IMAGE = "https://ispab.org/wp-content/themes/consultix/images/no-image-found-360x260.png";
 
     public String url(String path) {
         return url(path, original.name());
@@ -11,5 +12,23 @@ public class ImageUrl {
 
     public String url(String path, String size) {
         return BASE_IMAGE_URL + size + path;
+    }
+
+    public String fixUrl(String path) {
+        if (path == null || path.trim().isEmpty())
+            path = DEFAULT_IMAGE;
+
+        return fixUrl(path, DEFAULT_IMAGE, original.name());
+    }
+
+    public String fixUrl(String posterPath, String backdropPath, String size) {
+        String validPath = posterPath == null || posterPath.isEmpty() ?
+                backdropPath == null || backdropPath.isEmpty() ?
+                        DEFAULT_IMAGE : backdropPath : posterPath;
+
+
+        size = size == null || size.isEmpty() ? original.name() : size;
+
+        return BASE_IMAGE_URL + size + validPath;
     }
 }
